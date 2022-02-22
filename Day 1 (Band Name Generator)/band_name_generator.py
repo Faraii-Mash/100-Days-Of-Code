73bb092c1af7cf6e7dba46c490654b4c7cef6b35
Farai Mashiri <f.mashiri.mashiri@gmail.com>
2022-02-22 20:03:55 +0000

    added code to main py file.

---
+++ Day 1 (Band Name Generator)/band_name_generator.py
@@ -0,0 +1,12 @@
+# By FB Mashiri for #100Days of Code
+# A small band name generator 
+# This program uses user input to combine two inputs into a single string 
+
+# Print program name
+print('Welcome to a band name generator')
+# ask user for city name and save in city_name
+city_name = input('Please enter the name of the city you were born in below: \n')
+# ask user for pet name and save in pet_name
+pet_name = input('Please enter the name of a childhood pet you owned: \n')
+# join sting using concatenation 
+print('Your new band name maybe: ' + city_name + " " + pet_name )
