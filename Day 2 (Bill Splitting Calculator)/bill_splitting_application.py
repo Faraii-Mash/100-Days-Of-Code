527a3e32a8ac4db9146f025de725cc905cbb56e1
Farai Mashiri <f.mashiri.mashiri@gmail.com>
2022-02-23 19:15:35 +0000

    added comments describing the program

---
+++ Day 2 (Bill Splitting Calculator)/bill_splitting_application.py
@@ -0,0 +1,3 @@
+# By FB Mashiri for #100Days of Code
+# A bill splitting app
+# This program uses user input to combine to divide the bill by an input number of people including a tip by percentage 
