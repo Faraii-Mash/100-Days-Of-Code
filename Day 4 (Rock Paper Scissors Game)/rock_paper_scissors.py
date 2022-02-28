2d8426b8c35a814da9243c0fdb2ce9b9697ffb58
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-02-28 21:12:13 +0200

    Updated rock paper scissors code

--- Day 4 (Rock Paper Scissors Game)/rock_paper_scissors.py
+++ Day 4 (Rock Paper Scissors Game)/rock_paper_scissors.py
@@ -1,3 +1,8 @@
+# By FB Mashiri for #100Days of Code
+# A simple rock paper scissors application
+# This program uses user input to combine to divide the bill by an input number of people including a tip by percentage
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -26,4 +31,18 @@ scissors = '''
 '''
 
 #Rock Paper Scissors Program
-print('Weclome to the Rock Paper Scissors application')
+print('Welcome to the Rock Paper Scissors application')
+choice_pics = [rock, paper, scissors]
+
+user_in = int(input('Select either 0 for Rock, 1 for paper or 2 for Scissors. \n'))
+
+if user_in >= 3 or user_in < 0:
+    print('The number selected is an invalid input. Re-run the program and select 0,1 or 2')
+else:
+    print('You chose: ')
+    print(choice_pics[user_in])
+
+    comp_in = random.randint(0, 2)
+    print('The computer chose: ')
+    print(choice_pics[comp_in])
+
