04de3522ceecfe71bbd9284543e1ab579ccd22a9
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-01 13:41:47 +0200

    Made changes to description of the password_generator.py file

---
+++ Day 5 (Password Generator)/password_generator.py
@@ -0,0 +1,3 @@
+# By FB Mashiri for #100Days of Code
+# This Program is a password generator
+# This program uses user input to combine different inputs, loops, random module and string concatenation
