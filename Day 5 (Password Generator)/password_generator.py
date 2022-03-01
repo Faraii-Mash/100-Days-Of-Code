0055d4455fb0a0829113570b5126ce88c51d3f3c
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-01 21:40:04 +0200

    Updated code in the password_generator.py file

--- Day 5 (Password Generator)/password_generator.py
+++ Day 5 (Password Generator)/password_generator.py
@@ -1,3 +1,38 @@
 # By FB Mashiri for #100Days of Code
 # This Program is a password generator
 # This program uses user input to combine different inputs, loops, random module and string concatenation
+
+#  Password Generator Starts Here
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Python Password Generator.")
+num_letters_in = int(input("How many letters would you like in your password?\n"))
+num_symbols_in = int(input(f"How many symbols would you like?\n"))
+num_numbers_in = int(input(f"How many numbers would you like?\n"))
+
+# Generator
+password_list = []
+
+for char in range(1, num_letters_in + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, num_symbols_in + 1):
+    password_list += random.choice(symbols)
+
+for char in range(1, num_numbers_in + 1):
+    password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
