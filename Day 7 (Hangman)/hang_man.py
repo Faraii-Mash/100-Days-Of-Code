2d8f3d93d7546ef86477b138a230eb6f769c2816
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-04 21:18:04 +0200

    Hangman game development

--- Day 7 (Hangman)/hang_man.py
+++ Day 7 (Hangman)/hang_man.py
@@ -6,3 +6,24 @@ import random
 
 word_list_fn = 'words.txt'
 
+def wordlist():
+    #
+    print('Hangman word list loading')
+    #  inFile : open : read
+    in_file = open(word_list_fn, 'r')
+    # line to read
+    line = in_file.readline()
+    # word list Into list of string
+    word_list = line.split()
+    print(' ', len(word_list), 'words loaded')
+    return word_list
+
+def chosen_word(word_list):
+    return random.choice(word_list)
+
+
+word_list = wordlist()
+
+def
+
+
