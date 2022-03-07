82f3667a762b27fff8a4c312b868babb4d4edadf
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-07 22:14:45 +0200

    Made changes to code in ceasers_cipher.py files. added a working file for comparison

--- Day 8 (Ceaser's Cipher Program)/ceasers_cipher.py
+++ Day 8 (Ceaser's Cipher Program)/ceasers_cipher.py
@@ -2,6 +2,7 @@
 # This Program is a cipher encryption & decryption tool
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
+
 encrypted_text = ''
 decrypted_text = ''
 def greet_user(user_name):
