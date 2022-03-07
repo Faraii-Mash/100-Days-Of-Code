bb78e0cfd079ab8ad9258039f2395f7731e93505
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-07 21:08:54 +0200

    changed code in ceasers_cipher.py files.

--- Day 8 (Ceaser's Cipher Program)/ceasers_cipher.py
+++ Day 8 (Ceaser's Cipher Program)/ceasers_cipher.py
@@ -1,9 +1,12 @@
 # By FB Mashiri for #100Days of Code
 # This Program is a cipher encryption & decryption tool
 
-def greet_user():
+def greet_user(user_name):
+    print(f'Hello {user_name}')
     print('Welcome to the Ceasers Cipher')
     print('This program can be use to encrypt and decrypt Ciphers')
     print('Please select an option below to work on a cipher:')
 
-greet_user()
+
+user_name = input('Greetings, What is your name')
+greet_user(user_name)
