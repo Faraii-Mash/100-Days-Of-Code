b3359f3d91264e8e32a198d1e0182b6fd4606086
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-07 21:06:42 +0200

    added code to ceasers_cipher.py files.

---
+++ Day 8 (Ceaser's Cipher Program)/ceasers_cipher.py
@@ -0,0 +1,9 @@
+# By FB Mashiri for #100Days of Code
+# This Program is a cipher encryption & decryption tool
+
+def greet_user():
+    print('Welcome to the Ceasers Cipher')
+    print('This program can be use to encrypt and decrypt Ciphers')
+    print('Please select an option below to work on a cipher:')
+
+greet_user()
