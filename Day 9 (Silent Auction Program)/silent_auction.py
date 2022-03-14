1b149a62066c5731666af1215e90b0ba8e837bde
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-14 23:27:27 +0200

    working on day 9 Code

---
+++ Day 9 (Silent Auction Program)/silent_auction.py
@@ -0,0 +1,19 @@
+new_dictionary = {"Day": "Monday",
+                  "Week": "Week 2",
+                  "Year": "2022"}
+
+print(f'This is printing out of a dictionary'
+      f'The Day is  {new_dictionary["Day"]}\n'
+      f'In the {new_dictionary["Week"]}\n'
+      f'This is the year {new_dictionary["Year"]}\n')
+
+new_dictionary["Week"] = "Week 4 is the new entry"
+#create a new dictionary
+dict1 = {}
+
+print(new_dictionary)
+print(dict1)
+
+for things in new_dictionary:
+    print(things)
+    print(new_dictionary[things])
