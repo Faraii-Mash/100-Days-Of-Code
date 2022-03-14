f76b5c52b9fa47eff82ef2b7a3462e358b5ae1ad
Farai-Mashiri <f.mashiri.mashiri@gmail.com>
2022-03-15 00:21:20 +0200

    working on day 9 Code

--- Day 9 (Silent Auction Program)/silent_auction.py
+++ Day 9 (Silent Auction Program)/silent_auction.py
@@ -8,17 +8,36 @@ from art import logo
 def clear_screen():
     _ = system('clear')
 
+
+def highest_bid(bid_dict):
+    top_bid = 0
+    winning_bid = ''
+    for bidder in bid_dict:
+        bid_amount = bid_dict[bidder]
+    if bid_amount > top_bid:
+        top_bid = bid_amount
+        winning_bid = bidder
+    print(f'The winner is {winning_bid} with a bid of ${top_bid}')
+
 bid_dict ={}
 bidding_active = True
+
 #  Start program
 print(logo)
 
 while bidding_active:
     print('Welcome to the silent auction')
     name = input('Please enter your name below\n')
-    user_bid = input('Please enter the Bid amount below: \n$')
+    user_bid = int(input('Please enter the Bid amount below: \n$'))
 
-    prog_continue = input("Are there any other bidders ")
+    prog_continue = int(input("Are there any other bidders. Please enter:"
+                          "1: Yes "
+                          "2: No"))
 
-bid_dict[name] = user_bid
+    if prog_continue == 2:
+        bidding_active = False
+        highest_bid(bid_dict)
+    elif prog_continue == 1:
+        clear_screen()
+    bid_dict[name] = user_bid
 
